c951a9725cae6da11d0e284047b7d60be4df6cf3
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-06-10 15:09:52 +0530

    setup OrdersPage for Admin Dashboard and some bug fixes

--- frontend/src/components/admin/OrdersPage.jsx
+++ frontend/src/components/admin/OrdersPage.jsx
@@ -1,7 +1,144 @@
-import React from "react";
+import { server } from "@/main";
+import axios from "axios";
+import Cookies from "js-cookie";
+import React, { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import Loading from "../Loading";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import toast from "react-hot-toast";
 
 const OrdersPage = () => {
-  return <div>OrdersPage</div>;
+  const [orders, setOrders] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchOrders = async () => {
+    try {
+      const { data } = await axios.get(`${server}/api/order/admin/all`, {
+        headers: {
+          token: Cookies.get("token"),
+        },
+      });
+      setOrders(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  const updateOrderStatus = async (orderId, status) => {
+    setLoading(true);
+    try {
+      const { data } = await axios.post(
+        `${server}/api/order/${orderId}`,
+        { status },
+        {
+          headers: {
+            token: Cookies.get("token"),
+          },
+        }
+      );
+      toast.success(data.message);
+      fetchOrders();
+      setLoading(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setLoading(false);
+    }
+  };
+
+  const filteredOrders = orders.filter(
+    (order) =>
+      order.user.email.toLowerCase().includes(search.toLocaleLowerCase()) ||
+      order._id.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+  );
+  return (
+    <div className="p-6 space-y-6">
+      <h1 className="text-2xl font-bold">Manage Orders</h1>
+
+      <Input
+        placeholder="search by email or order id"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="w-full md:w-1/2"
+      />
+
+      {loading ? (
+        <Loading />
+      ) : filteredOrders.length > 0 ? (
+        <div className="overflow-x-auto">
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Order Id</TableHead>
+                <TableHead>User Email</TableHead>
+                <TableHead>Total</TableHead>
+                <TableHead>Status</TableHead>
+                <TableHead>Date</TableHead>
+                <TableHead>Section</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {filteredOrders.map((order) => (
+                <TableRow key={order._id}>
+                  <TableCell>
+                    <Link to={`/order/${order._id}`}>{order._id}</Link>
+                  </TableCell>
+                  <TableCell>{order.user.email}</TableCell>
+                  <TableCell>{order.subTotal}</TableCell>
+                  <TableCell>
+                    <span
+                      className={`px-2 py-1 rounded text-white ${
+                        order.status === "Pending"
+                          ? "bg-yellow-500"
+                          : order.status === "Shipped"
+                          ? "bg-blue-500"
+                          : "bg-green-500"
+                      }`}
+                    >
+                      {order.status}
+                    </span>
+                  </TableCell>
+                  <TableCell>
+                    {moment(order.createdAt).format("DD MM YYYY")}
+                  </TableCell>
+                  <TableCell>
+                    <select
+                      value={order.status}
+                      className="w-[150px] px-3 py-2 border rounded-md"
+                      onChange={(e) =>
+                        updateOrderStatus(order._id, e.target.value)
+                      }
+                    >
+                      <option value="Pending">Pending</option>
+                      <option value={"Shipped"}>Shipped</option>
+                      <option value={"Delivered"}>Delivered</option>
+                    </select>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      ) : (
+        <p>No Orders</p>
+      )}
+    </div>
+  );
 };
 
 export default OrdersPage;
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -79,14 +79,20 @@ const ProductPage = () => {
         </div>
       )}
       {relatedProduct?.length > 0 && (
-        <div className="mt-12">
-          <h2 className="text-xl font-bold ">Related Products</h2>
-          <div className=" mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
-            {relatedProduct.map((e) => (
-              <ProductCard key={e._id} product={e} />
-            ))}
-          </div>
-        </div>
+        <>
+          {loading ? (
+            <Loading />
+          ) : (
+            <div className="mt-12">
+              <h2 className="text-xl font-bold ">Related Products</h2>
+              <div className=" mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+                {relatedProduct.map((e) => (
+                  <ProductCard key={e._id} product={e} />
+                ))}
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
