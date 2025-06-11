b1362ab8387f6dbd36b2fb07a8d2a7d909099795
tyagisarthak13 <sarthaktyagi797@gmail.com>
2025-06-11 23:58:29 +0530

    setup Update products feature for ADMIN

--- frontend/src/pages/OrderPage.jsx
+++ frontend/src/pages/OrderPage.jsx
@@ -1,6 +1,7 @@
 import Loading from "@/components/Loading";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { UserData } from "@/context/UserContext";
 import { server } from "@/main";
 import axios from "axios";
 import Cookies from "js-cookie";
@@ -11,6 +12,8 @@ const OrderPage = () => {
   const { id } = useParams();
   const [order, setOrder] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  const { user } = UserData();
 
   const navigate = useNavigate();
 
@@ -49,101 +52,114 @@ const OrderPage = () => {
   }
   return (
     <div className="container mx-auto py-6 px-4">
-      <Card className="mb-6">
-        <CardHeader>
-          <div className="flex justify-between">
-            <CardTitle className="text-2xl font-bold">Order Details</CardTitle>
-            <Button onClick={() => window.print()}>Print Order</Button>
-          </div>
-        </CardHeader>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
-          <div>
-            <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
-            <p>
-              <strong>Order Id: </strong>
+      {user._id === order.user._id || user.role === "admin" ? (
+        <>
+          <Card className="mb-6">
+            <CardHeader>
+              <div className="flex justify-between">
+                <CardTitle className="text-2xl font-bold">
+                  Order Details
+                </CardTitle>
+                <Button onClick={() => window.print()}>Print Order</Button>
+              </div>
+            </CardHeader>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-4">
+              <div>
+                <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
+                <p>
+                  <strong>Order Id: </strong>
 
-              {order._id}
-            </p>
-            <p>
-              <strong>Status: </strong>
-              <span
-                className={`${
-                  order.status === "Pending"
-                    ? "text-yellow-500"
-                    : "text-green-500"
-                }`}
-              >
-                {order.status}
-              </span>
-            </p>
-            <p>
-              <strong>Total Items: </strong>
+                  {order._id}
+                </p>
+                <p>
+                  <strong>Status: </strong>
+                  <span
+                    className={`${
+                      order.status === "Pending"
+                        ? "text-yellow-500"
+                        : "text-green-500"
+                    }`}
+                  >
+                    {order.status}
+                  </span>
+                </p>
+                <p>
+                  <strong>Total Items: </strong>
 
-              {order.items.length}
-            </p>
+                  {order.items.length}
+                </p>
 
-            <p>
-              <strong>Payment Method: </strong>
+                <p>
+                  <strong>Payment Method: </strong>
 
-              {order.method}
-            </p>
-            <p>
-              <strong>SubTotal: </strong>
+                  {order.method}
+                </p>
+                <p>
+                  <strong>SubTotal: </strong>
 
-              {order.subTotal}
-            </p>
-            <p>
-              <strong>Placed At: </strong>
+                  {order.subTotal}
+                </p>
+                <p>
+                  <strong>Placed At: </strong>
 
-              {new Date(order.createdAt).toLocaleDateString()}
-            </p>
+                  {new Date(order.createdAt).toLocaleDateString()}
+                </p>
 
-            <p>
-              <strong>Paid At: </strong>
+                <p>
+                  <strong>Paid At: </strong>
 
-              {order.paidAt || "Payment Through COD"}
-            </p>
-          </div>
+                  {order.paidAt || "Payment Through COD"}
+                </p>
+              </div>
 
-          <div>
-            <h2 className="text-xl font-semibold mb-4">Shipping Details</h2>
-            <p>
-              <strong>Phone:</strong> {order.phone}
-            </p>
-            <p>
-              <strong>Address:</strong> {order.address}
-            </p>
-            <p>
-              <strong>User:</strong> {order.user?.email || "Guest"}
-            </p>
-          </div>
-        </div>
-      </Card>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {order.items.map((e, i) => (
-          <Card key={i}>
-            <Link to={`/product/${e.product._id}`}>
-              <img
-                src={e.product.images[0]?.url}
-                alt={e.product.title}
-                className="max-w-full max-h-full object-contain"
-              />
-            </Link>
-
-            <CardContent>
-              <h3 className="text-lg font-semibold">{e.product.title}</h3>
-              <p>
-                <strong>Quantity:</strong>
-                {e.quantity}
-              </p>
-              <p>
-                <strong>Price:</strong>₹{e.product.price}
-              </p>
-            </CardContent>
+              <div>
+                <h2 className="text-xl font-semibold mb-4">Shipping Details</h2>
+                <p>
+                  <strong>Phone:</strong> {order.phone}
+                </p>
+                <p>
+                  <strong>Address:</strong> {order.address}
+                </p>
+                <p>
+                  <strong>User:</strong> {order.user?.email || "Guest"}
+                </p>
+              </div>
+            </div>
           </Card>
-        ))}
-      </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {order.items.map((e, i) => (
+              <Card key={i}>
+                <Link to={`/product/${e.product._id}`}>
+                  <img
+                    src={e.product.images[0]?.url}
+                    alt={e.product.title}
+                    className="max-w-full max-h-full object-contain"
+                  />
+                </Link>
+
+                <CardContent>
+                  <h3 className="text-lg font-semibold">{e.product.title}</h3>
+                  <p>
+                    <strong>Quantity:</strong>
+                    {e.quantity}
+                  </p>
+                  <p>
+                    <strong>Price:</strong>₹{e.product.price}
+                  </p>
+                </CardContent>
+              </Card>
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className="text-red-500 text-3xl text-center">
+          This is not your order <br />
+          <Link to={"/"} className="mt-4 underline text-blue-400">
+            Go to home page
+          </Link>
+        </p>
+      )}
     </div>
   );
 };
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -8,10 +8,17 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { CartData } from "@/context/CartContext";
 import { ProductData } from "@/context/ProductContext";
 import { UserData } from "@/context/UserContext";
-import React, { useEffect } from "react";
+import { categories, server } from "@/main";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { Edit, Loader, X } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
 
 const ProductPage = () => {
@@ -20,7 +27,7 @@ const ProductPage = () => {
 
   const { id } = useParams();
 
-  const isAuth = UserData();
+  const { isAuth, user } = UserData();
 
   useEffect(() => {
     fetchProduct(id);
@@ -30,12 +37,168 @@ const ProductPage = () => {
     addToCart(id);
   };
 
+  const [show, setShow] = useState(false);
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [stock, setStock] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+
+  const [btnLoading, setBtnLoading] = useState(false);
+
+  const updateHandler = () => {
+    setShow(!show);
+    setCategory(product.category);
+    setTitle(product.title);
+    setDescription(product.description);
+    setStock(product.stock);
+    setPrice(product.price);
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setBtnLoading(true);
+
+    try {
+      const { data } = await axios.put(
+        `${server}/api/product/${id}`,
+        { title, description, price, stock, category },
+        {
+          headers: {
+            token: Cookies.get("token"),
+          },
+        }
+      );
+
+      toast.success(data.message);
+      fetchProduct(id);
+      setShow(false);
+      setBtnLoading(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setBtnLoading(false);
+    }
+  };
+
+  const [updatedImages, setUpdatedImages] = useState(null);
+
+  const handleSubmitImage = async (e) => {
+    e.preventDefault();
+    setBtnLoading(true);
+
+    if (!updatedImages || updatedImages.length === 0) {
+      toast.error("Please select new images.");
+      setBtnLoading(false);
+      return;
+    }
+
+    const formData = new FormData();
+
+    for (let i = 0; i < updatedImages.length; i++) {
+      formData.append("files", updatedImages[i]);
+    }
+
+    try {
+      const { data } = await axios.post(
+        `${server}/api/product/${id}`,
+        formData,
+        {
+          headers: {
+            token: Cookies.get("token"),
+          },
+        }
+      );
+
+      toast.success(data.message);
+      fetchProduct(id);
+      setBtnLoading(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setBtnLoading(false);
+    }
+  };
+
   return (
     <div>
       {loading ? (
         <Loading />
       ) : (
         <div className="w-full max-w-screen-xl mx-auto px-4 py-8">
+          {user && user.role === "admin" && (
+            <div className="w-[300px] md:w-[450px] m-auto mb-5">
+              <Button onClick={updateHandler}>{show ? <X /> : <Edit />}</Button>
+              {show && (
+                <div className="space-y-4">
+                  <div>
+                    <Label>Title</Label>
+                    <Input
+                      placeholder="Product Title"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <Label>Description</Label>
+                    <Input
+                      placeholder="Product Description"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <Label>Category</Label>
+                    <select
+                      value={category}
+                      onChange={(e) => setCategory(e.target.value)}
+                      required
+                      className="w-full p-2 border rounded-md dark:bg-gray-900 dark:text-white"
+                    >
+                      {categories.map((e) => (
+                        <option value={e} key={e}>
+                          {e}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+
+                  <div>
+                    <Label>Price</Label>
+                    <Input
+                      placeholder="Product Price"
+                      type="number"
+                      value={price}
+                      onChange={(e) => setPrice(e.target.value)}
+                      required
+                    />
+                  </div>
+
+                  <div>
+                    <Label>Stock</Label>
+                    <Input
+                      placeholder="Product Stock"
+                      type="number"
+                      value={stock}
+                      onChange={(e) => setStock(e.target.value)}
+                      required
+                    />
+                  </div>
+                  <Button
+                    type="submit"
+                    className="w-full"
+                    disabled={btnLoading}
+                    onClick={submitHandler}
+                  >
+                    {btnLoading ? <Loader /> : "Update Product"}
+                  </Button>
+                </div>
+              )}
+            </div>
+          )}
           {product && (
             <div className="flex flex-col lg:flex-row items-start gap-14">
               <div className="w-[290px] md:w-[650px]">
@@ -55,7 +218,30 @@ const ProductPage = () => {
                   <CarouselPrevious />
                   <CarouselNext />
                 </Carousel>
+                {user && user.role === "admin" && (
+                  <form
+                    onSubmit={handleSubmitImage}
+                    className="flex flex-col gap-4"
+                  >
+                    <div>
+                      <Label>Upload New Images:</Label>
+                      <input
+                        type="file"
+                        name="files"
+                        id="files"
+                        multiple
+                        accept="image/*"
+                        onChange={(e) => setUpdatedImages(e.target.files)}
+                        className="block w-full mt-1 text-sm"
+                      />
+                    </div>
+                    <Button type="submit" disabled={btnLoading}>
+                      Update Image
+                    </Button>
+                  </form>
+                )}
               </div>
+
               <div className="w-full lg:w-1/2 space-y-4">
                 <h1 className="text-2xl font-bold">{product.title}</h1>
                 <p className="text-lg">{product.description}</p>
